e92e2d968daf6ba72f7274d6fbaf48f86e16e9eb
Bobby K Bose <57045659+bobby-bose@users.noreply.github.com>
2022-11-10 09:42:58 +0530

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -126,4 +126,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "bobbykboseoffice@gmail.com"
-EMAIL_HOST_PASSWORD = "ukbmzanlsguqjuty"
+EMAIL_HOST_PASSWORD = "ukbmzanlsgu******"
